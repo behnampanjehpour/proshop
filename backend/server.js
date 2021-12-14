fa0d158106ddc48e3e80702575c80320058d81dd
Behnam Panjehpour <behnam@gmail.com>
2021-12-14 16:23:50 -0500

    'again3'

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
-import express from 'express'
 import path from 'path'
-import colors from 'colors'
+import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import morgan from 'morgan'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
@@ -16,7 +17,7 @@ connectDB()
 
 const app = express()
 
-if (process.env.NODE.ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
@@ -30,8 +31,10 @@ app.use('/api/upload', uploadRoutes)
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
@@ -40,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
   )
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running')
+    res.send('API is running....')
   })
 }
 
@@ -48,9 +51,10 @@ app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
+
 app.listen(
   PORT,
   console.log(
-    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 )
